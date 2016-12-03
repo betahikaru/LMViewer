373a10a175f00dba3523d23072008b634c077964
betahikaru <beta.hikaru2000+github@gmail.com>
2016-12-03 19:57:08 +0900

    Initial Commit

---
+++ LMViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMViewer
+//
+//  Created by 鈴木 光 on 2016/12/03.
+//  Copyright © 2016年 betahikaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMViewer
+//
+//  Created by 鈴木 光 on 2016/12/03.
+//  Copyright © 2016年 betahikaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
